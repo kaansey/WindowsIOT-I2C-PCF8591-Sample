607efd344aa55559df706642cb8f7712548b145b
Kaan Seyitogullari <kaanse@hotmail.com>
2016-08-02 11:47:42 +0300

    .

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Devices.SerialCommunication;
+using System.Text;
+using Windows.Storage.Streams;
+using Windows.Devices.Enumeration;
+using System.Collections.ObjectModel;
+using Windows.Devices.Gpio;
+using Windows.Devices.Spi;
+using Windows.UI.Core;
+using Windows.Devices.I2c;
+using System.Threading.Tasks;
+
+
+
+
+
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private DispatcherTimer timer;
+        SensorHelper sh = new SensorHelper();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            constructor();
+           // init();
+        }
+
+        void constructor()
+        {
+            sh.InitI2C();
+            this.timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            Unloaded += MainPage_Unload;
+        }
+
+        private void MainPage_Unload(object sender, RoutedEventArgs e)
+        {
+            sh.dispose();
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            int distance = sh.getADC(0);
+            textBox.Text = Convert.ToString(distance);
+            if (distance>120)
+            {
+               // writeSerial("tag.read_id(2)");
+            }
+        }
+
+
+    
+
+        private void ScrolltoBottom(TextBox textBox)
+        {
+            var grid = (Grid)VisualTreeHelper.GetChild(textBox, 0);
+            for (var i = 0; i < VisualTreeHelper.GetChildrenCount(grid); i++)
+            {
+                object obj = VisualTreeHelper.GetChild(grid, i);
+                if (!(obj is ScrollViewer)) continue;
+                //((ScrollViewer)obj).ChangeView(0.0f, ((ScrollViewer)obj).ExtentHeight, 1.0f);
+                ((ScrollViewer)obj).ScrollToVerticalOffset(((ScrollViewer)obj).ExtentHeight);
+                break;
+            }
+        }
+
+      
+    }
+}
---
+++ App1/SensorHelper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.I2c;
+using Windows.UI.Xaml;
+
+namespace App1
+{
+    class SensorHelper
+    {
+        private const byte PDCF8591 = (0x90 >> 1);
+        public const byte PDCF8591_ADC_CH0 = 0x40;
+        public const byte PDCF8591_ADC_CH1 = 0x41;
+        public const byte PDCF8591_ADC_CH2 = 0x42;
+        public const byte PDCF8591_ADC_CH3 = 0x43;
+
+        private I2cDevice I2CPCF8591;
+
+        const long referenceMv = 5000;
+        const int TABLE_ENTRIES = 12;
+        const int NBR_ELEMS = 10;
+        const int firstElement = 250;
+
+        const int INTERVAL = 250;
+        static int[] distance = { 150, 140, 130, 100, 60, 50, 40, 35, 30, 25, 20, 15 };
+
+
+        public async void InitI2C()
+        {
+            string acs = I2cDevice.GetDeviceSelector();
+            var dls = await DeviceInformation.FindAllAsync(acs);
+
+            if (dls.Count() == 0)
+            {
+                return;
+            }
+
+            var settings = new I2cConnectionSettings(PDCF8591);
+            settings.BusSpeed = I2cBusSpeed.StandardMode;
+            I2CPCF8591 = await I2cDevice.FromIdAsync(dls[0].Id, settings);
+
+            if (I2CPCF8591 == null)
+            {
+                return;
+            }
+
+        }
+
+        public int getADC(int ch)
+        {
+            byte[] recv = new byte[2];
+
+            if (ch ==0)
+            I2CPCF8591.WriteRead(new byte[] { PDCF8591_ADC_CH0 }, recv);
+            else if (ch == 0)
+                I2CPCF8591.WriteRead(new byte[] { PDCF8591_ADC_CH1 }, recv);
+            else if(ch == 0)
+                I2CPCF8591.WriteRead(new byte[] { PDCF8591_ADC_CH2 }, recv);
+            else if(ch == 0)
+                I2CPCF8591.WriteRead(new byte[] { PDCF8591_ADC_CH3 }, recv);
+
+            return recv[1];
+        }
+
+        public void dispose()
+        {
+            I2CPCF8591.Dispose();
+        }
+
+
+    }
+}
